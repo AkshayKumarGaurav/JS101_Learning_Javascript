030b55935db451e809462de2ec3426dd46039299
Akshay <akshaykumargaurav@gmail.com>
2022-10-11 17:21:46 +0530

    files added

---
+++ Day1_assignments/problem4.js
@@ -0,0 +1,19 @@
+/*Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+Print the report card of the student (You can make it look nice by using some keyboard symbols )
+Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration*/
+let name="🅰🅺🆂🅷🅰🆈";
+console.log(name);
+let school="🇲​​​​​🇦​​​​​🇸​​​​​🇦​​​​​🇮​​​​​ 🇸​​​​​🇨​​​​​🇭​​​​​🇴​​​​​🇴​​​​​🇱​​​​​";
+console.log(school);
+let grade="𝓐";
+console.log(grade);
+let section="ᗩ";
+console.log(section);
+let rollno="𝕗𝕨𝟚𝟛_𝟘𝟘𝟛𝟚";
+console.log(rollno);
+let physics="70";
+console.log(physics);
+let math="90";
+console.log(math);
+let chemistry="78";
+console.log(chemistry);
